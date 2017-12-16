f3c043dbf153037900b93e187c925aa888cddfad
fletcherist <romanovexports@gmail.com>
2017-12-16 15:06:38 +0300

    [env]: setup redux

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ import { Router } from 'preact-router'
 import { Link } from 'preact-router'
 
 import { createHashHistory } from 'history'
+import { Provider, coonect } from 'preact-redux'
+import { createStore } from 'redux'
 
-console.log(<Link />)
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment': return {...state}
+    default: return state
+  }
+}
+
+const store = createStore(reducer, {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const App = () => (
   <div>
-    hello wawawssss
-    <a href='#/nav'>go to nav</a>
-    <Router history={createHashHistory()}>
-      {/*<Nav path='nav' />*/}
-    </Router>
+    <Provider store={store}>
+      hello wawawssss
+      <a href='#/nav'>go to nav</a>
+      <Router history={createHashHistory()}>
+        {/*<Nav path='nav' />*/}
+      </Router>
+    </Provider>
   </div>
 )
 
