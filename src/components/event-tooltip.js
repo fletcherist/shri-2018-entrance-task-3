f386853ead7bebceefc4a5561962a30ff615b735
fletcherist <romanovexports@gmail.com>
2017-12-19 01:46:14 +0300

    [feature]: implemented event-tooltip

--- src/components/event-tooltip.js
+++ src/components/event-tooltip.js
@@ -7,16 +7,18 @@ import { EmptyDivider } from './divider'
 
 const rule = state => ({
   maxWidth: '360px',
-  boxShadow: '0 1px 16px 0 rgba(0,44,92,0.28);',
+  position: 'relative',
+  boxShadow: '0 1px 16px 0 rgba(0,44,92,0.28)',
   padding: '10px 16px',
   zIndex: '9999',
-  position: 'relative',
   borderRadius: '8px',
-  ':after': {
-    content: ' ',
-    backgroundColor: 'blue',
-    width: '10px',
-    height: '10px',
+  '::after': {
+    content: '""',
+    position: 'absolute',
+    top: '-20px',
+    left: '61.8%', // Golden rule damn it
+    border: '10px solid white',
+    borderColor: 'transparent transparent white transparent'
   }
 })
 
@@ -26,15 +28,27 @@ const editIconRule = state => ({
 
 const editButtonRule = state => ({
   backgroundColor: '#E9ECEF',
-  height: '32px',
-  width: '32px',
+  height: '24px',
+  width: '24px',
   borderRadius: '16px',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   cursor: 'pointer',
   position: 'absolute',
+  top: '16px',
   right: '16px'
+})
+
+const participantsRule = state => ({
+  display: 'flex',
+  alignItems: 'center',
+  flexBasis: '30px'
+})
+
+const otherParticipantsRule = state => ({
+  marginLeft: '6px',
+  color: '#858E98'
 })
 
 const EventTooltip = ({
@@ -44,16 +58,22 @@ const EventTooltip = ({
     <div className={styles.editButtonRule}>
       <img className={styles.editIconRule} src={`dist/${EditIcon}`} />
     </div>
-
     <TextHeadline marginTop={8} marginBottom={8}>Рассуждения о высоком</TextHeadline>
     <Text>14 декабря, 15:00—17:00  · Ржавый Фред</Text>
     <EmptyDivider height={16} />
-    <User userName='Дарт Вейдер' userPhoto={'https://1ofdmq2n8tc36m6i46scovo2e-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/Steven_Hallam-slide.jpg'} />
+    <div className={styles.participantsRule}>
+      <User userName='Дарт Вейдер' userPhoto={'https://1ofdmq2n8tc36m6i46scovo2e-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/Steven_Hallam-slide.jpg'} />
+      <div className={styles.otherParticipantsRule}>
+        <Text> и 12 участников</Text>
+      </div>
+    </div>
   </div>
 )
 
 export default connect({
   rule,
   editButtonRule,
-  editIconRule
+  editIconRule,
+  participantsRule,
+  otherParticipantsRule
 })(EventTooltip)
